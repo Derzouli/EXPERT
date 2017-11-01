e64375253b254819d2506ef0e041fa79e028ee51
Sebastien CHONG <schong@e3r4p1.42.fr>
2017-11-01 20:10:49 +0100

    evolution parsing

--- expert.py
+++ expert.py
@@ -3,46 +3,108 @@
 from pyparsing import Group, Literal, Optional, Regex, Word, ZeroOrMore, Upcase, alphas
 from collections import Counter
 from pyparsing import ParseException
-
-# 1 PLUS
-# 2 NOT
-# 3 OR
-# 4 XOR
+import argparse
+import sys
+import re
 
 def grammar():
 	_and = Literal('+').setParseAction(lambda:'+')
 	_not = Literal('!').setParseAction(lambda:'!')
 	_or =  Literal('|').setParseAction(lambda:'|')
 	_xor =  Literal('^').setParseAction(lambda:'^')
-	sign = _and | _not | _or | _xor
-	letter = Word(alphas, exact=1)
-	atom = letter | Optional(_not) + letter('not')
-	monomial = Optional(sign('sign')) + atom
-	polynomial = Group(monomial) + ZeroOrMore(monomial)
+	sign = _and | _or | _xor
+	letter = Upcase(Word(alphas, max=1))
+	atom = letter
+	monomial = Optional(_not) + atom + Optional(sign)
+	polynomial = Group(monomial + ZeroOrMore(monomial))
 	return polynomial('left') + '=>' + polynomial('right')
-
 
 _grammar = grammar()
 
-def parse(s):
-	try:
-		equation = _grammar.parseString(s)
-	except ParseException:
-		print "NIQUE TA MERE"
-		return (-1);
-	series = Counter()
-	print equation
-	# for side, terms in ((+1, equation.left), (-1, equation.right)):
-	# 	for t in terms:
-	# 		n = side * t.get('joiner', 1) * t.get('sign', 1) * t.get('coeff', 1)
-	# 		series[t.get('exponent', 0)] += n
-	# try:
-	# 	degree = max(e for e, c in series.items() if c)
-	# except ValueError as e:
-	# 	print("Every numbers belongs to the set of real numbers are solutions")
-	# 	return 0
-	# return [series[e] for e in range(degree, -1, -1)]
+class Rules(object):
 
+	def __init__(self):
+		self.data = {}
+		self.count = 0
+		self.character = [chr(ord('A')+x) for x in range(26)]
+		self.temp = dict((el, 0) for el in self.character)
+		self.goal = {}
+
+	def parse(self, s):
+		try:
+			s_with_blank = ''.join(s.split())
+			if (s_with_blank.find('<=>') > 0):
+				str_temp = s_with_blank.replace("<=>", "=>")
+				stack = [[], "=>", []]
+				equation = _grammar.parseString(str_temp)
+				stack[0].extend(equation.left)
+				stack[2].extend(equation.right)
+				self.data.update({self.count:stack})
+				self.count += 1;
+				stack = [[], "=>", []]
+				stack[0].extend(equation.right)
+				stack[2].extend(equation.left)
+				self.data.update({self.count:stack})
+				self.count += 1;
+			else:
+				equation = _grammar.parseString(s_with_blank)
+				stack = [[], "=>", []]
+				stack[0].extend(equation.right)
+				stack[2].extend(equation.left)
+				self.data.update({self.count:stack})
+				self.count += 1;
+		except ParseException:
+			print len(s)
+			print "Parsing Error"
+			sys.exit()
+
+	def set_initial(self, line):
+		for c in line:
+			if (c.isupper() and c.isalpha()):
+				self.temp[c] = 1
+
+	def set_goal(self, line):
+		for c in line:
+			if (c.isupper() and c.isalpha()):
+				self.goal[c] = 1
+
+
+parser = argparse.ArgumentParser(description='Expert System: Solver')
+parser.add_argument('files', nargs='*')
+
+def main(filename):
+	rules = Rules()
+	file = open(filename, "r")
+	for line in file.readlines():
+		sharp_pos = line.find("#")
+		equal_pos = line.find("=")
+		mark_pos = line.find("?")
+		len_line = len(line)
+		if (len_line == 1):
+			continue
+		else:
+			if (equal_pos == 0):
+				rules.set_initial(line[1:])
+			if (mark_pos == 0):
+				rules.set_goal(line[1:])
+			if (equal_pos < 0 and mark_pos < 0) or \
+			(equal_pos > 0 and mark_pos < 0) :
+				if (sharp_pos > 0):
+					rules.parse(line.split("#")[0])
+				elif (sharp_pos < 0):
+					rules.parse(line)
+	if (len(rules.goal) == 0):
+		print "No Goal"
+		sys.exit()
+	if (len(rules.data) == 0):
+		print "No Rule"
+		sys.exit()
+	print rules.data
 
 if __name__ == "__main__":
-	parse("AC | B => C")
+	args = parser.parse_args()
+	if (len(args.files) >= 1):
+		main(args.files[0])
+	else:
+		print "Incorrect Argument"
+
