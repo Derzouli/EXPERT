1239fbfd625db8738e7c00201cb21458c3e5a247
Sebastien CHONG <schong@e3r8p20.42.fr>
2017-11-23 05:20:23 +0100

    lexical

--- graph.py
+++ graph.py
@@ -18,6 +18,33 @@ class Graph:
 
 		return visited
 
+	def backtrack(self,goal):
+		paths = self.find_path(goal)
+		if (len(paths) == 0):
+			return 1
+		val = paths.pop()
+		if (val.value):
+			return 2
+		return self.backtrack(val)
+
+	def setpath(self,v):
+
+		visited = []
+		for key, value in self.graph.iteritems():
+			if (v in value) and (key is not visited):
+				visited.append(key)
+
+		for x in visited:
+			self.graph[x.setValue(True)]
+		return visited
+
+	def settrack(self,goal):
+		paths = self.setpath(goal)
+		if (len(paths) == 0):
+			return 0
+		val = paths.pop()
+		self.settrack(val)
+
 	def show(self):
 		for key, value in self.graph.iteritems():
-			print key, value
+			print key, key.value
--- lexical.py
+++ lexical.py
@@ -12,6 +12,13 @@ class Term(object):
 	def getName(self):
 		return self.name
 
+	def setValue(self, value):
+		self.value = value
+		return self
+
+	def getValue(self):
+		return self.value
+
 	def __str__(self):
 		return self.name
 
@@ -31,11 +38,11 @@ class Term(object):
 		and len(self.name) == len(other.name)
 
 # applique sur chaque element du tuple la fonction de decomposition de term
-def map_tuple_gen(func, tup):
-	return tuple(func(itup) for itup in tup)
+def map_tuple_gen(func, tup, initial_fact):
+	return tuple(func(itup, initial_fact) for itup in tup)
 
 # decompose chaque term avec la data passee en parametre
-def build_term(data):
+def build_term(data, initial_fact):
 	d = collections.deque(data)
 	t = 0
 	stack_operator = []
@@ -47,11 +54,20 @@ def build_term(data):
 			if (stack_operator):
 				op = stack_operator.pop(0)
 				if (op == '+'):
-					t = t + Term(s)
+					if s in initial_fact:
+						t = t + Term(s, value=True)
+					else:
+						t = t + Term(s)
 				elif (op == '^'):
-					t = t + Term(s)
+					if s in initial_fact:
+						t = t + Term(s, value=True)
+					else:
+						t = t + Term(s)
 			else:
-				t = Term(s)
+				if s in initial_fact:
+					t = Term(s, value=True)
+				else:
+					t = Term(s)
 	return (t)
 
 def build_graph(lst_left, lst_right):
@@ -61,7 +77,7 @@ def build_graph(lst_left, lst_right):
 	return graph
 
 def transform(rules):
-	trans_list = [map_tuple_gen(build_term, item) for item in rules.data]
+	trans_list = [map_tuple_gen(build_term, item, rules.initial_fact) for item in rules.data]
 	tuple_left, tuple_right = zip(*trans_list)
 	lst_left, lst_right = list(tuple_left), list(tuple_right)
 	for key, val in enumerate(lst_left):
@@ -71,4 +87,10 @@ def transform(rules):
 		except ValueError:
 			pass
 	graph = build_graph(lst_left, lst_right)
-	graph.find_path(Term('C'))
+	if (graph.backtrack(Term('K')) == 2):
+		graph.settrack(Term('K'))
+	graph.show()
+	# toto = graph.find_path(Term('H'))
+	# for key, value in enumerate(toto):
+	# 	print value, value.value
+	# print Term('C', value=True).value
